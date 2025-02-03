2a3846b37f5dc0a6c8a60e52ace30e5de3755d63
Nili Scharf <nilischarf@gmail.com>
2025-02-03 16:53:08 +0200

    complete plantsy coding challenge

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant, onUpdatePlant, onDeletePlant }) {
+  const [isSoldOut, setIsSoldOut] = useState(false)
+  const [price, setPrice] = useState(plant.price)
+
+  function handleUpdatePrice(e) {
+    const newPrice = e.target.value
+    setPrice(newPrice)
+
+    fetch(`http://localhost:6001/plants/${plant.id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ price: newPrice })
+    })
+      .then((response) => response.json())
+      .then(onUpdatePlant)
+  }
+
+  function handleDelete() {
+    fetch(`http://localhost:6001/plants/${plant.id}`, {
+      method: "DELETE"
+    })
+      .then(() => onDeletePlant(plant.id))
+  }
+
+  function handleStockToggle() {
+    setIsSoldOut(!isSoldOut)
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={plant.image} alt={plant.name}/>
+      <h4>{plant.name}</h4>
+      <p>
+        Price: $<input type="number" value={price} onChange={handleUpdatePrice} step="0.01"/> 
+      </p>
+      <button 
+        className={isSoldOut ? "" : "primary"}
+        onClick={handleStockToggle}
+      >
+        {isSoldOut ? "Out of Stock" : "In Stock"}
+      </button>
+      <button onClick={handleDelete}>🗑 Delete</button>
     </li>
   );
 }
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants }) {
+  const [plantList, setPlantList] = useState(plants)
+
+function handleDeletePlant(id) {
+  setPlantList(plantList.filter((plant) => plant.id !== id))
+}
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plantList.map((plant) => (
+        <PlantCard key={plant.id} plant={plant} onDeletePlant={handleDeletePlant} /> 
+      ))}
+    </ul>
   );
 }
 
